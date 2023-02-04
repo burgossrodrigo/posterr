ec1135da61fed5b77715ebaf1ebd6661bee12a0a
burgossrodrigo <drigo@tangleswap.exchange>
2023-02-04 01:31:58 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Posts, Users } from './data'
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SinglePage from './pages';
+
+
+
 
 function App() {
+
+  useEffect(() => {
+    if(localStorage.Posts === undefined){
+      localStorage.setItem('Posts', JSON.stringify(Posts))
+    }
+
+    if(localStorage.Users === undefined){
+      localStorage.setItem('Users', JSON.stringify(Users))
+    }
+    console.log(localStorage.Users)
+
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>        
+        <Route path="/" element={<SinglePage />} />
+        <Route path="/following" element={<SinglePage />} />
+        <Route path="/user/:id" element={<SinglePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
